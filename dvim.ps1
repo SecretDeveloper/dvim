c7104462184e6f07944a013c94161371b6a7d5be
Gary Kenneally <gkenneally@unum.com>
2023-07-19 15:09:12 +0100

    using docker hosted image

--- dvim.ps1
+++ dvim.ps1
@@ -1,22 +1,23 @@
 param (
-    [Parameter(Mandatory=$false)]
-    [string]$path=(Get-Location).Path
+    [Parameter(Mandatory = $false)]
+    [string]$path = (Get-Location).Path
 )
 
 if (Test-Path -Path $path) {
     if (Get-Item -Path $path -ea SilentlyContinue) {
         if ((Get-Item -Path $path).PSIsContainer) {
             # Folder provided
-            docker run --rm -ti -v "${path}:/home" dvim
+            docker run --rm -ti -v "${path}:/home" secretdeveloper/dvim:latest
         }
         else {
             # File path provided
             $folderPath = Split-Path -Path $path
             $filePath = Split-Path -Path $path -Leaf
-            docker run --rm -ti -v "${folderPath}:/home" dvim "${filePath}"
+            docker run --rm -ti -v "${folderPath}:/home" secretdeveloper/dvim:latest "${filePath}"
         }
     }
-} else {
+}
+else {
     Write-Output "The specified path does not exist."
 }
 
